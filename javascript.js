cba4eff15cbe5c2d2db2161825fd0cf0c2d67ab7
Davin <286156958+nivad320-lgtm@users.noreply.github.com>
2026-06-03 12:25:18 +0900

    Fix Bug, double equal and divide by 0

--- javascript.js
+++ javascript.js
@@ -17,6 +17,9 @@ function operate(num1,operator,num2) {
             return num1 * num2; 
             break;
         case '/':
+            if (num2 == 0) {
+                return "ERROR";
+            }
             return num1 / num2; 
             break;
     }
@@ -44,12 +47,16 @@ currentValueContainer.appendChild(operatorSign);
 currentValueContainer.appendChild(numTwoSign);
 currentValueContainer.appendChild(totalSign);
 
-// Calculator Display
+// Calculator Number  Display
 const calculatorDisplay = document.createElement('p');
 calculatorDisplay.innerText = `0`;
 
-displayContainer.appendChild(calculatorDisplay);
+// Calculator Current Operator Display
+const operatorDisplay = document.createElement('p');
+operatorDisplay.innerText = "\u00A0";
 
+displayContainer.appendChild(operatorDisplay);
+displayContainer.appendChild(calculatorDisplay);
 
 // Create 10 Buttons for digits
 for (let i = 0; i < 10; i++) {
@@ -81,14 +88,28 @@ const operatorButtonArray = [...operatorButtonNodeList]
 // Changes the value of the operator
 for (const operatorButton of operatorButtonArray) {
     operatorButton.addEventListener("click", (event) => {
+        if (num2) {
+            total = operate(num1, operator, num2)
+            totalSign.innerText = `Total: ${total}`
+            operatorDisplay.innerText = operatorSign.innerText;
+            calculatorDisplay.innerText = totalSign.innerText;
+            num1 = total;
+            numOneSign.innerText = `First Number: ${num1}`;
+            num2 = '';
+            numTwoSign.innerText = `Second Number: ${num2}`;
+        }
         operator = event.target.innerText;
         operatorSign.innerText = `Operator: ${operator}`;
-        calculatorDisplay.innerText = operatorSign.innerText;
+        operatorDisplay.innerText = operatorSign.innerText;
+        
     }); 
 }
 
 const equalButton = document.querySelector('.equal');
 equalButton.addEventListener("click", () => {
+    if (!num1||!num2) {
+        return 
+    }
     total = operate(num1, operator, num2);
     totalSign.innerText = `Total: ${total}`
     calculatorDisplay.innerText = totalSign.innerText;
